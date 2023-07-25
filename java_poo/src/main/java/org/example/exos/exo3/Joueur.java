5f7dfd5132bb9aa6f308f8fef226b18522763f7a
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-07-25 16:22:13 +0200

    java_poo : added exo3 and exo4 (not finished)

---
+++ java_poo/src/main/java/org/example/exos/exo3/Joueur.java
@@ -0,0 +1,56 @@
+package org.example.exos.exo3;
+
+public class Joueur {
+
+    private String nom;
+    private int niveau;
+    private int experience;
+
+    public Joueur() {}
+
+    // Les valeurs de niveau et expérience sont mises par défaut
+    public Joueur(String nom) {
+        this.nom = nom;
+        this.niveau = 1;
+        this.experience = 0;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        System.out.print(this.nom + " change de pseudo pour : " + nom + "\n");
+        this.nom = nom;
+    }
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    // Pas de setters pour le niveau et l'expérience car leur valeurs ne changent qu'en cas de quête
+
+    @Override
+    public String toString() {
+        return nom + ", niveau " + niveau + ", " + experience + " xp";
+    }
+
+    public void effectuerUneQuete() {
+        this.experience += 15;
+        System.out.println("Quête accomplie ! " + nom + " gagne 15 xp (Total : " + experience + " xp)");
+        if (this.experience >= 100) {
+            monterDeNiveau();
+        }
+    }
+
+    private void monterDeNiveau() {
+        this.experience -= 100;
+        this.niveau += 1;
+        System.out.println("Montée de niveau : " + nom + " passe niveau " + this.niveau + " !");
+    }
+
+}
---
+++ java_poo/src/main/java/org/example/exos/exo4/WaterTank.java
@@ -0,0 +1,70 @@
+package org.example.exos.exo4;
+
+public class WaterTank {
+
+    private double netWeight;
+    private int maxCapacity;
+    private double level;
+
+    private static double totalVolume;
+
+    public WaterTank() {}
+
+    public WaterTank(double netWeight, int maxCapacity) {
+        this.netWeight = netWeight;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public double getNetWeight() {
+        return netWeight;
+    }
+
+    public void setNetWeight(double netWeight) {
+        this.netWeight = netWeight;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public double getLevel() {
+        return level;
+    }
+
+    public void setLevel(double level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Citerne : " + level + "/" + maxCapacity + " L ; poids à vide : " + netWeight + " kg";
+    }
+
+    public void fill(double volume) {
+        if (level + volume <= maxCapacity) {
+           level += volume;
+           totalVolume += volume;
+        } else {
+            double realVolume = volume - maxCapacity - level;
+            System.out.println("La cuve est pleine, " + (volume - realVolume) + " L d'eau n'ont pas pû être ajoutés");
+            totalVolume += realVolume;
+            level = maxCapacity;
+        }
+    }
+
+    public void empty(double volume) {
+        if (level - volume >= 0) {
+            level -= volume;
+            totalVolume -= volume;
+        } else {
+            System.out.println("La cuve est vide, " + (volume - level) + " L d'eau n'ont pas pû être retirés");
+            totalVolume -= level;
+            level = 0;
+        }
+    }
+
+}
