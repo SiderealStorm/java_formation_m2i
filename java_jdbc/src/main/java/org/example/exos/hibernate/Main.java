7c73bf9f5b3cf118a1ca0bef6c8ddc6585ca530a
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-08-28 13:35:38 +0200

    hibernate : added exercise

---
+++ java_jdbc/src/main/java/org/example/exos/hibernate/Main.java
@@ -0,0 +1,102 @@
+package org.example.exos.hibernate;
+
+import org.example.exos.hibernate.entity.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        // Préparation de la session :
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+        // Ouverture de la session :
+        Session session = sessionFactory.openSession();
+        
+        // Création de 5 produits
+        Product product1 = new Product();
+        product1.setBrand("Test 1");
+        product1.setReference("Référence test 1");
+        product1.setPurchaseDate(new Date());
+        product1.setPrice(15.99);
+        product1.setStock(12);
+
+        Product product2 = new Product();
+        product2.setBrand("Test 2");
+        product2.setReference("Référence test 2");
+        product2.setPurchaseDate(new Date());
+        product2.setPrice(159.99);
+        product2.setStock(1);
+
+        Product product3 = new Product();
+        product3.setBrand("Test 3");
+        product3.setReference("Référence test 3");
+        product3.setPurchaseDate(new Date());
+        product3.setPrice(5.99);
+        product3.setStock(112);
+
+        Product product4 = new Product();
+        product4.setBrand("Test 4");
+        product4.setReference("Référence test 4");
+        product4.setPurchaseDate(new Date());
+        product4.setPrice(165.99);
+        product4.setStock(2);
+
+        Product product5 = new Product();
+        product5.setBrand("Test 5");
+        product5.setReference("Référence test 5");
+        product5.setPurchaseDate(new Date());
+        product5.setPrice(10.99);
+        product5.setStock(25);
+
+        // Ajout des 5 produits en BDD
+
+        session.getTransaction().begin();
+        System.out.println("=== Ajout produits ===");
+        session.save(product1);
+        session.save(product2);
+        session.save(product3);
+        session.save(product4);
+        session.save(product5);
+
+        session.getTransaction().commit();
+
+        // Affichage du produit 2
+
+        Product product = session.load(Product.class, 2);
+        System.out.println("=== Affichage produit 2 ===");
+        System.out.println(product);
+
+        // Suppression du produit 3
+
+        session.getTransaction().begin();
+
+        product = session.load(Product.class, 3);
+        System.out.println("=== Suppression du produit " + product.getId() + " ===");
+
+        session.delete(product);
+
+        session.getTransaction().commit();
+
+        // Modification du produit 1
+
+        session.getTransaction().begin();
+
+        product = session.load(Product.class, 1);
+        System.out.println("=== Modification du produit " + product.getId() + " ===");
+        product.setReference("Référence modifiée de test 1");
+
+        session.update(product);
+
+        session.getTransaction().commit();
+        
+        // Fermeture de Session et SessionFactory
+        session.close();
+        sessionFactory.close();
+    }
+}
---
+++ java_jdbc/src/main/java/org/example/exos/hibernate/entity/Product.java
@@ -0,0 +1,81 @@
+package org.example.exos.hibernate.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String brand;
+    private String reference;
+
+    @Column(name = "purchase_date")
+    private Date purchaseDate;
+    private double price;
+    private int stock;
+
+    public Product() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", reference='" + reference + '\'' +
+                ", purchaseDate=" + purchaseDate +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
+    }
+}
