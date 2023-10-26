c06d2b991216e4b0327eacd7dec6066f44a8b4b8
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-10-26 13:59:23 +0200

    typescript : updated exo1

--- js_typescript/exos/exo1/src/assets/js/main.mjs
+++ js_typescript/exos/exo1/src/assets/js/main.mjs
@@ -1,19 +1,27 @@
-const myForm = document.querySelector("form");
+var _a;
+// Eléments HTML utilisés
 const firstNameInput = document.querySelector("input#firstname");
 const lastNameInput = document.querySelector("input#lastname");
 const firstNameError = document.querySelector("span#firstname-error");
 const lastNameError = document.querySelector("span#lastname-error");
-myForm === null || myForm === void 0 ? void 0 : myForm.addEventListener("submit", (event) => {
+const greetings = document.querySelector("h2#greetings");
+// Action sur le bouton du formulaire
+(_a = document.querySelector("form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", (event) => {
     event.preventDefault();
+    // Remise à zéro des champs
     firstNameError.textContent = "";
     lastNameError.textContent = "";
-    const firstName = firstNameInput.value;
-    const lastName = lastNameInput.value;
-    if (firstName && lastName) {
+    greetings.textContent = "";
+    // Récupération des champs du formulaire
+    const firstName = firstNameInput.value.trim();
+    const lastName = lastNameInput.value.trim();
+    if (firstName != "" && lastName != "") {
+        // Affichage HTML et console du nom-prénom
         console.log(`Bonjour ${firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase()} ${lastName.toUpperCase()} !`);
-        document.querySelector("h2#greetings").textContent = `Bonjour ${firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase()} ${lastName.toUpperCase()} !`;
+        greetings.textContent = `Bonjour ${firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase()} ${lastName.toUpperCase()} !`;
     }
     else {
+        // Affichage HTML et console des messages d'erreurs
         if (!firstName) {
             console.log("Veuillez saisir votre prénom");
             firstNameError.textContent = "Champ obligatoire";
--- js_typescript/exos/exo1/src/assets/ts/main.mts
+++ js_typescript/exos/exo1/src/assets/ts/main.mts
@@ -1,24 +1,31 @@
-const myForm = document.querySelector("form");
-
+// Eléments HTML utilisés
 const firstNameInput = document.querySelector("input#firstname") as HTMLInputElement;
 const lastNameInput = document.querySelector("input#lastname") as HTMLInputElement;
 const firstNameError = document.querySelector("span#firstname-error")!;
 const lastNameError = document.querySelector("span#lastname-error")!;
+const greetings = document.querySelector("h2#greetings")!;
 
-myForm?.addEventListener("submit", (event) => {
+// Action sur le bouton du formulaire
+document.querySelector("form")?.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    
+    // Remise à zéro des champs
     firstNameError.textContent = "";
-    lastNameError.textContent = ""
+    lastNameError.textContent = "";
+    greetings.textContent = "";
 
-    const firstName = firstNameInput.value;
-    const lastName = lastNameInput.value;
+    // Récupération des champs du formulaire
+    const firstName = firstNameInput.value.trim();
+    const lastName = lastNameInput.value.trim();
 
-    if (firstName && lastName) {
+    if (firstName != "" && lastName != "") {
+        // Affichage HTML et console du nom-prénom
+
         console.log(`Bonjour ${firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase()} ${lastName.toUpperCase()} !`);
 
-        document.querySelector("h2#greetings")!.textContent = `Bonjour ${firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase()} ${lastName.toUpperCase()} !`;
+        greetings.textContent = `Bonjour ${firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase()} ${lastName.toUpperCase()} !`;
     } else {
+        // Affichage HTML et console des messages d'erreurs
         if (!firstName) {
             console.log("Veuillez saisir votre prénom");
 
