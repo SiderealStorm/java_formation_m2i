128282a0a9af03a7c78b89559d2e6343b798ca7a
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-11-08 12:05:23 +0100

    angular : added demos and exercise on routing

---
+++ js_angular/exos/exo13/README.md
@@ -0,0 +1,7 @@
+# Exercice Angular Routing A
+
+Créer une application Angular disposant d'un routing et possédant 4 pages:
+- Une page d'accueil via /
+- Une page de contacts via /contacts
+- Une page d'informations via /about
+- Une page d'erreur 404 pour toutes les autres routes
---
+++ js_angular/exos/exo13/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactHomePageComponent } from './contact/pages/contact-home-page/contact-home-page.component';
+import { AboutPageComponent } from './about/pages/about-page/about-page.component';
+import { Error404pageComponent } from './pages/error/error404page/error404page.component';
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'contacts', component: ContactHomePageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: '**', component: Error404pageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
