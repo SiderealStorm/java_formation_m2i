dc9279c14174cee3946940c92fbdf89a8fe4b858
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-11-08 16:50:10 +0100

    angular : added more to routing demo

--- js_angular/demos/demo-routing-gen/src/app/app.module.ts
+++ js_angular/demos/demo-routing-gen/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProductListingComponent } from './products/pages/product-listing/product-listing.component';
+import { ProductDetailsComponent } from './products/pages/product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    ProductListingComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
--- js_angular/demos/demo-routing-gen/src/app/services/data.service.ts
+++ js_angular/demos/demo-routing-gen/src/app/services/data.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Product } from '../models/Products.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +8,4 @@ import { Injectable } from '@angular/core';
 export class DataService {
 
   testValue = "Default";
-
-  constructor() { }
 }
